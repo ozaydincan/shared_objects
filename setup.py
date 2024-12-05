6daee1a42ea5a6a43b8eaddf30261fa4947967bc
ozaydincan <ozaydincan.app@gmail.com>
2024-12-05 17:38:58 +0100

    Zed Topic usage

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="shared_objects",  # Replace with a unique name for PyPI
-    version="0.1.0",
+    version="0.1.1",
     description="ROS2 Utils",
     author="Can Ozaydin",
     author_email="ozaydincan.app@gmail.com",
